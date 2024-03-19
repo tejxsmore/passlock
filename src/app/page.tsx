1180a77d65a593809d81a4c255bd175c01890cbf
tejxsmore <tejasmore1104@gmail.com>
2024-03-19 21:38:10 +0530

    Fixed errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
 import Navbar from "../components/Navbar";
-import { neon } from "@neondatabase/serverless";
-const sql = neon(process.env.DATABASE_URL);
 
 type Props = {};
 
 export default async function Home(props: Props) {
-  const response = await sql`SELECT version()`;
-  // console.log(response);
-
   return (
     <div className="bg-dark text-light min-h-screen">
       <Navbar />
